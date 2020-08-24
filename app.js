3fed0e164a7ddf6b2650ad3bb3d697d4bdefd306
James Lees <jamestrevorlees@gmail.com>
2020-08-24 06:18:14 +0200

    add event listeners function and add event listeners for the form submit

---
+++ app.js
@@ -0,0 +1,22 @@
+// Book constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+// UI Constructor
+function UI() {}
+
+// Event listeners
+document.getElementById("book-form").addEventListener("submit", function (e) {
+
+  // Get form values
+  const title = document.getElementById('title').value,
+        author = document.getElementById('author').value,  
+        isbn = document.getElementById('isbn').value  
+
+
+
+  e.preventDefault();
+});
