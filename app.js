1de82e9fff70bf47545a08e6cc4b747d67334cf3
James Lees <jamestrevorlees@gmail.com>
2020-08-24 07:31:00 +0200

    add clear fields function to clear input fields once submitted

--- app.js
+++ app.js
@@ -6,17 +6,50 @@ function Book(title, author, isbn) {
 }
 
 // UI Constructor
-function UI() {}
+function UI() {
+  //vAdd book to list
+  UI.prototype.addBookToList = function (book) {
+    const list = document.getElementById("book-list");
+    // Create a tr element
+    const row = document.createElement("tr");
+    // Insert cols
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a  href="#" class="delete">X</a></td>
+    `;
+    list.appendChild(row);
+  };
+}
+
+// Clear Fields
+UI.prototype.clearFields = function () {
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("isbn").value = "";
+};
 
 // Event listeners
 document.getElementById("book-form").addEventListener("submit", function (e) {
-
   // Get form values
-  const title = document.getElementById('title').value,
-        author = document.getElementById('author').value,  
-        isbn = document.getElementById('isbn').value  
+  const title = document.getElementById("title").value,
+    author = document.getElementById("author").value,
+    isbn = document.getElementById("isbn").value;
 
+  // Instantiate a book
 
+  const book = new Book(title, author, isbn);
+
+  // Instantiate UI
+
+  const ui = new UI();
+
+  // Add book to list
+  ui.addBookToList(book);
+
+  // Clear fields
+  ui.clearFields();
 
   e.preventDefault();
 });
