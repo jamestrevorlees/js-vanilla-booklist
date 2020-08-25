f8c89bbdd53deef00cd8f88cf1359b90a3bffb5e
James Lees <jamestrevorlees@gmail.com>
2020-08-25 05:03:47 +0200

    create alert structure and UI

--- app.js
+++ app.js
@@ -21,14 +21,34 @@ function UI() {
     `;
     list.appendChild(row);
   };
-}
 
-// Clear Fields
-UI.prototype.clearFields = function () {
-  document.getElementById("title").value = "";
-  document.getElementById("author").value = "";
-  document.getElementById("isbn").value = "";
-};
+  // Show Alert
+  UI.prototype.showAlert = function (message, className) {
+    // Create div
+    const div = document.createElement("div");
+    //Add classes
+    div.className = `alert ${className}`;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector(".container");
+    // Get form
+    const form = document.querySelector("#book-form");
+    // Insert alert
+    container.insertBefore(div, form);
+    // Timeout after 3 seconds
+    setTimeout(function () {
+      document.querySelector(`.alert`).remove();
+    }, 3000);
+  };
+
+  // Clear Fields
+  UI.prototype.clearFields = function () {
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("isbn").value = "";
+  };
+}
 
 // Event listeners
 document.getElementById("book-form").addEventListener("submit", function (e) {
@@ -45,11 +65,21 @@ document.getElementById("book-form").addEventListener("submit", function (e) {
 
   const ui = new UI();
 
-  // Add book to list
-  ui.addBookToList(book);
+  // Validate
 
-  // Clear fields
-  ui.clearFields();
+  if (title === "" || author === "" || isbn === "") {
+    // Error alert
+    ui.showAlert("Please fill in all fields", "error");
+  } else {
+    // Add book to list
+    ui.addBookToList(book);
+
+    // Show success
+    ui.showAlert("Book Added!", "success");
+
+    // Clear fields
+    ui.clearFields();
+  }
 
   e.preventDefault();
 });
