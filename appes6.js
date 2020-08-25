57c5f42ddef9d5a5ad6a9120457416e04cbbcdb5
James Lees <jamestrevorlees@gmail.com>
2020-08-25 06:21:10 +0200

    add local storage

--- appes6.js
+++ appes6.js
@@ -54,6 +54,51 @@ class UI {
   }
 }
 
+// Local storage class
+
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem("books") === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem("books"));
+    }
+
+    return books;
+  }
+
+  static displayBooks() {
+    const books = Store.getBooks();
+    books.forEach(function (book) {
+      const ui = new UI();
+
+      //Add book to UI
+      ui.addBookToList(book);
+    });
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+    books.forEach(function (book, index) {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+}
+
+// DOM load event
+document.addEventListener("DOMContentLoaded", Store.displayBooks);
+
 // Event listeners for books
 document.getElementById("book-form").addEventListener("submit", function (e) {
   // Get form values
@@ -78,6 +123,9 @@ document.getElementById("book-form").addEventListener("submit", function (e) {
     // Add book to list
     ui.addBookToList(book);
 
+    // Add to local storage
+    Store.addBook(book);
+
     // Show success
     ui.showAlert("Book Added!", "success");
 
@@ -93,7 +141,11 @@ document.getElementById("book-list").addEventListener("click", function (e) {
   // Instantiate UI
   const ui = new UI();
 
+  // Delete book
   ui.deleteBook(e.target);
+
+  // Remove from LS
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
   // Show message
   ui.showAlert("Book removed!", "success");
